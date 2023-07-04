ec65ca554dedda05d2e6842196fabf797c2272e6
bhathiya <bhathiyabandara87@gmail.com>
2023-07-04 16:36:36 +0530

    Final Commit - Redesigned the Application into Object Oriented

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 import selectorlib  # used to only extract particular information
-from sendemail import send_email
+import sendemail
 import time
 import sqlite3
 
@@ -11,53 +11,60 @@ HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) '
                   'Chrome/39.0.2171.95 Safari/537.36'}
 
-connection = sqlite3.connect('data.db')  # Connecting to the DB
-cursor = connection.cursor()  # cursor object serves as the object that can execute the queries
+
+class Event:
+
+    def scraper(self, url):
+        """Scrape the page source from the URL"""
+        response = requests.get(url, headers=HEADERS)
+        text = response.text
+        return text
+
+    def extract(self, source):
+        """Extracting the Content We want"""
+        extractor = selectorlib.Extractor.from_yaml_file('extract.yaml')
+        value = extractor.extract(source)['tours']
+        return value
 
 
-def scraper(url):
-    """Scrape the page source from the URL"""
-    response = requests.get(url, headers=HEADERS)
-    text = response.text
-    return text
+class Database:
 
+    def __init__(self):
+        self.connection = sqlite3.connect('data.db')  # Connecting to the DB
+        # self.cursor = connection.cursor()  # cursor object serves as the object that can execute the queries
 
-def extract(source):
-    """Extracting the Content We want"""
-    extractor = selectorlib.Extractor.from_yaml_file('extract.yaml')
-    value = extractor.extract(source)['tours']
-    return value
+    def store(self, extracted):
+        row = extracted.split(',')
+        row = [item.strip() for item in row]
 
+        cursor = self.connection.cursor()
+        cursor.execute("INSERT INTO events VALUES (?,?,?)", row)
+        self.connection.commit()
 
-def store(extracted):
-    row = extracted.split(',')
-    row = [item.strip() for item in row]
+    def read(self, extracted):
+        row = extracted.split(',')
+        row = [item.strip() for item in row]
+        band, city, date = row
 
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO events VALUES (?,?,?)", row)
-    connection.commit()
-
-
-def read(extracted):
-    row = extracted.split(',')
-    row = [item.strip() for item in row]
-    band, city, date = row
-
-    cursor.execute("SELECT * FROM events WHERE band = ? AND city = ? AND date = ?,", (band, city, date))
-    rows = cursor.fetchall()
-    print(rows)
-    return rows
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM events WHERE band = ? AND city = ? AND date = ?,", (band, city, date))
+        rows = cursor.fetchall()
+        print(rows)
+        return rows
 
 
 if __name__ == '__main__':
     while True:
-        scraped = scraper(URL)
-        extracted = extract(scraped)
+        event = Event()
+        scraped = event.scraper(URL)
+        extracted = event.extract(scraped)
         print(extracted)
 
         if extracted != 'No upcoming tours':
-            row = read(extracted)
+            database = Database()
+            row = database.read(extracted)
             if not row:
-                store(extracted)
-                send_email(message='Hey! New Event was Found')
+                database.store(extracted)
+                email = sendemail.Email()
+                email.send_email(message='Hey! New Event was Found')
         time.sleep(1000)
